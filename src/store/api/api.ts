92abd4d56ad23582d47fa854650209bb65ff4fd4
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2024-06-02 22:12:19 +0500

    new server link

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,7 +1,7 @@
 import {BaseQueryFn, createApi, FetchArgs, fetchBaseQuery, FetchBaseQueryError} from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://85.10.216.104/api/',
+    baseUrl: 'https://restaurants.zapto.org/api/',
     prepareHeaders: (headers) => {
         const token =  localStorage.getItem('token');
         if(token){
